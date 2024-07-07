74ef2cc7a8f85e997ad49077b14507bddb488098
doğukan KORKMAZ <dogukankorkmaz42@hotmail.com>
2024-07-07 21:53:20 +0300

    sinema bileti evet hayıt
    sinema bileti almak istermisiniz yoksa istemezmisiniz

---
+++ sinema bileti evet hayır/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Bilet fiyatları
+int VIPBiletFiyati = 50;
+int NormalBiletFiyati = 30;
+
+// Kullanıcıdan bilet sayısı ve tipi isteniyor
+Console.Write("VIP bilet sayısı: ");
+int VIPBiletSayisi = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Normal bilet sayısı: ");
+int NormalBiletSayisi = Convert.ToInt32(Console.ReadLine());
+
+// Toplam bilet fiyatı hesaplanıyor
+int toplamFiyat = VIPBiletSayisi * VIPBiletFiyati + NormalBiletSayisi * NormalBiletFiyati;
+
+// Kullanıcıya toplam fiyat gösteriliyor
+Console.WriteLine("Toplam fiyat: " + toplamFiyat + " TL");
+
+// Ödeme işlemi gerçekleştiriliyor
+Console.Write("Ödeme yapmak istediğinizden emin misiniz? (E/H)");
+string cevap = Console.ReadLine();
+
+if (cevap.ToLower() == "e")
+{
+    Console.WriteLine("Ödeme işlemi gerçekleştirildi.");
+    Console.WriteLine("Biletleriniz e-posta yoluyla gönderilecektir. İyi seyirler!");
+}
+else
+{
+    Console.WriteLine("İşlem iptal edildi.");
+}
+
+Console.ReadLine();
